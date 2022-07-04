6b6ebd99084c0827f17ee4da2fee2bdbadaea408
IgnacioJC <igncanton.dev@gmail.com>
2022-07-04 10:25:52 -0300

    navBar created

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import './NavBar.css'
+import logoNav from '../../logo.svg'
+
+const NavBar = () => {
+    return (
+        <nav className="navBar">
+            <div className="logoNav">
+                <a href={"#"}><img src={logoNav} alt="Logo" /></a>
+            </div>
+            <ul>
+                <li><a href={"www.google.com"} target="_blank">Home</a></li>
+                <li><a href={"#"}>Products</a></li>
+                <li><a href={"#"}>About</a></li>
+                <li><a href={"#"}>Contact</a></li>
+            </ul>
+        </nav>);
+}
+
+export default NavBar;
